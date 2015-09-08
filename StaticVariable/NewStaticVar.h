4b782c4065e3198dfba34b0007dc43b3561d9e3b
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-09-08 16:25:24 +0530

    Create NewStaticVar.h

---
+++ StaticVariable/NewStaticVar.h
@@ -0,0 +1,15 @@
+//
+//  NewStaticVar.h
+//  PracticeObjectiveC
+//
+//  Created by Aseem 1 on 08/09/15.
+//  Copyright (c) 2015 codeBrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewStaticVar : NSObject
+- (void) increment;
+- (void) print;
+
+@end
