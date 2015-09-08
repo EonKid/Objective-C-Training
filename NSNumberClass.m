573b9ebe97b2c604803c98f40baf97175dadb577
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-09-08 18:20:49 +0530

    Update NSNumberClass.m

--- NSNumberClass.m
+++ NSNumberClass.m
@@ -19,6 +19,21 @@ int main(int argc , const char *argv[]){
         int x = [ integerOne integerValue];
         float y = [ integerTwo floatValue ];
         NSLog(@"Value of x = %i and y = %f\n",x,y);
+         if ([integerOne isEqualToNumber:integerTwo] == YES) {
+            NSLog(@"They are equal !!");
+            
+        }else {
+        
+            NSLog(@"They are not equal !!");
+        }
+        if ([integerOne compare:integerTwo]==NSOrderedAscending) {
+            
+            NSLog(@"The number one is less than second !!");
+        }else {
+        
+            NSLog(@"The number one is greater than second !!");
+        }
+
         
     }
     return 0;
