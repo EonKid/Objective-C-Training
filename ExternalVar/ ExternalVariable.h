057f6cb17e48dc4bf64185ac1ea1e51daa9acd76
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-09-08 16:02:53 +0530

    Create  ExternalVariable.h

---
+++ ExternalVar/ ExternalVariable.h
@@ -0,0 +1,14 @@
+//
+//  ExternalVariable.h
+//  PracticeObjectiveC
+//
+//  Created by Dhruv Narayan Singh 1 on 08/09/15.
+//  Copyright (c) 2015 codeBrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExternalVariable : NSObject
+- (void) ChangeExternalVar;
+
+@end
