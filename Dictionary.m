95627c71e9d3363405ffad22d36a2c5726917797
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-09-09 14:40:39 +0530

    Create Dictionary.m

---
+++ Dictionary.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  PracticeObjectiveC
+//
+//  Created by Dhruv Narayan Singh  on 09/09/15.
+//  Copyright (c) 2015 codeBrew. All rights reserved.
+//
+#import<Foundation/Foundation.h>
+#define  N 25
+
+int main(int argc , const char *argv[]){
+
+    @autoreleasepool{
+    
+        NSMutableDictionary *myDictionary =[NSMutableDictionary dictionary];
+        [myDictionary setObject:@"Red Color"
+                         forKey:@"apple"];
+        [myDictionary setObject:@"Yellow Color"
+                         forKey:@"mango"];
+        [myDictionary setObject:@"Dark Green Color"
+                         forKey:@"pineapple"];
+        [myDictionary setObject:@"Light Color"
+                         forKey:@"grapes"];
+        NSLog(@"Fruits you wanted to search where stored objects of Dictionary are \"%@\"  \n",[myDictionary objectForKey:@"apple"]);
+        
+    }
+    
+    return 0;
+
+}
